677d5f8001ef8bb5a12ce5e97ea6cdc765d47995
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-06-14 21:21:30 -0700

    init commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Average Joe Burger');
+INSERT INTO burgers (burger_name) VALUES ('Boom Boom to the Moon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Double with Extra Trouble Burger');
---
+++ public/assets/js/app.js
@@ -0,0 +1,14 @@
+  
+$(document).ready(function () {
+    $(".devour").on("submit", function (event) {
+      event.preventDefault();
+      var id = $(this).children(".id").val();
+      console.log(id);
+      $.ajax({
+        method: "PUT",
+        url: "/burgers/" + id,
+      }).then(function (data) {
+        location.reload();
+      });
+    });
+  });
